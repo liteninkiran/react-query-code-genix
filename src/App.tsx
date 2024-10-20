a5650385278794eae7d62311ec9db7c495228359
Kiran Anand <liteninkiran@hotmail.com>
2024-10-20 09:51:44 +0100

    Old Way

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>App</div>;
-}
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/todos')
+      .then((res) => setData(res.data))
+      .catch(console.log);
+  }, []);
+  return <div>{JSON.stringify(data)}</div>;
+};
 
 export default App;
