111969b185199d93cca39873c43398a4fb2f21c7
Kiran Anand <liteninkiran@hotmail.com>
2024-10-20 20:41:04 +0100

    Difference Between Query State and Query Function Status

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,18 +2,12 @@ import { useTodosIds } from '../services/queries';
 
 export const Todos = () => {
   const todosIdsQuery = useTodosIds();
-  console.log(todosIdsQuery.data);
-  if (todosIdsQuery.isPending) {
-    return <span>Loading...</span>;
-  }
-
-  if (todosIdsQuery.isError) {
-    return <span>Error!</span>;
-  }
 
   return (
     <>
-      {todosIdsQuery.data.map((id) => (
+      <p>Query Function Status: {todosIdsQuery.fetchStatus}</p>
+      <p>Query Function Status: {todosIdsQuery.status}</p>
+      {todosIdsQuery.data?.map((id) => (
         <p key={id}>{id}</p>
       ))}
     </>
