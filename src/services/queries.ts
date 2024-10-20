012416e45554e477cfc1b33ec890444830696bdf
Kiran Anand <liteninkiran@hotmail.com>
2024-10-20 20:52:14 +0100

    Parallel Queries

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,9 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
-import { getTodosIds } from './api';
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { getTodo, getTodosIds } from './api';
 
-export function useTodosIds() {
+export const useTodosIds = () => {
   return useQuery({
     queryKey: ['todos'],
     queryFn: getTodosIds,
+  });
+}
+
+export function useTodos(ids: (number | undefined)[] | undefined) {
+  return useQueries({
+    queries: (ids ?? []).map((id) => ({
+      queryKey: ['todo', { id }],
+      queryFn: () => getTodo(id!),
+    })),
   });
 }
