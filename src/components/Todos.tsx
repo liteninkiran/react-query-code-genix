951d0541d0c5301dffba252e925d3501b220a62e
Kiran Anand <liteninkiran@hotmail.com>
2024-10-20 20:42:30 +0100

    useIsFetching Hook

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,15 @@
+import { useIsFetching } from '@tanstack/react-query';
 import { useTodosIds } from '../services/queries';
 
 export const Todos = () => {
   const todosIdsQuery = useTodosIds();
+  const isFetching = useIsFetching();
 
   return (
     <>
       <p>Query Function Status: {todosIdsQuery.fetchStatus}</p>
       <p>Query Function Status: {todosIdsQuery.status}</p>
+      <p>Global isFetching: {isFetching}</p>
       {todosIdsQuery.data?.map((id) => (
         <p key={id}>{id}</p>
       ))}
