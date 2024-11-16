36b21c1bb153f243710456d9ce310937a00babfc
Kiran Anand <liteninkiran@hotmail.com>
2024-11-16 15:37:08 +0000

    Update Todo Using Mutation

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,5 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useCreateTodo } from '../services/mutations';
+import { useCreateTodo, useUpdateTodo } from '../services/mutations';
 import { useTodos, useTodosIds } from '../services/queries';
 import { Todo } from '../types/todo';
 
@@ -8,11 +8,18 @@ export const Todos = () => {
   const todosQueries = useTodos(todosIdsQuery.data ?? []);
 
   const createTodoMutation = useCreateTodo();
+  const updateTodoMutation = useUpdateTodo();
 
   const { register, handleSubmit } = useForm<Todo>();
 
   const handleCreateTodoSubmit: SubmitHandler<Todo> = (data) => {
     createTodoMutation.mutate(data);
+  };
+
+  const handleMarkAsDoneSubmit = (data: Todo | undefined) => {
+    if (data) {
+      updateTodoMutation.mutate({ ...data, checked: true });
+    }
   };
 
   return (
@@ -39,6 +46,14 @@ export const Todos = () => {
                   <strong>Title:</strong> {data?.title},{' '}
                   <strong>Description:</strong> {data?.description},{' '}
                 </span>
+                <div>
+                  <button
+                    onClick={() => handleMarkAsDoneSubmit(data)}
+                    disabled={data?.checked}
+                  >
+                    {data?.checked ? 'Done' : 'Mark as done'}
+                  </button>
+                </div>
               </li>
             )
         )}
